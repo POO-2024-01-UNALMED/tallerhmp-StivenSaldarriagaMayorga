d80e34de2248179da338c575c0c644249c218f5b
StivenSaldarriagaMayorga <ssaldarriagam@unal.edu.co>
2024-08-26 20:16:18 -0500

    Prueb2

--- futbolista/Futbolista.py
+++ futbolista/Futbolista.py
@@ -32,6 +32,4 @@ class Futbolista(Deportista):
 
     # Método __str__ para imprimir la información del futbolista
     def __str__(self):
-        return (f"Mi nombre es {self.getNombre()} soy profesional en el deporte "
-                f"{self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo "
-                f"{self.getAnosPracticando()} años en el deporte")
+        return (f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAnosPracticando()} años en el deporte")
